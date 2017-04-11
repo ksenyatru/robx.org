988c23428bc89de0f05b270cd22a72a4b629f197
Gleb Svechnikov <gleb.svechnikov@gmail.com>
2017-04-11 23:23:59 +0400

    Temporary commented resize function in address

--- scripts/addresses.js
+++ scripts/addresses.js
@@ -3,7 +3,7 @@ function add_elements_addresses() {
 
 	$('main > .addresses > .list > div').each(function() {
 		url = 'https://www.google.ru/maps/place/' + encodeURIComponent($(this).children('div:first-child').text()) + ',+Санкт-Петербург'
-		
+
 		$(this).append('<a class = "circled" href = "' + url + '">Посмотреть на карте</a>');
 	});
 }
@@ -16,27 +16,27 @@ function remove_elements_addresses() {
 
 function init_addresses() {
 	if(window.matchMedia('(max-width: 480px)').matches) {
-		$('.addresses').get(0).mobile = true;
+	//	$('.addresses').get(0).mobile = true;
 
 		add_elements_addresses();
 	} else {
-		$('.addresses').get(0).mobile = false;
+//		$('.addresses').get(0).mobile = false;
 	}
 }
 
 function resize_addresses() {
-	if(window.matchMedia('(max-width: 480px)').matches) {
-		if($('.addresses').get(0).mobile == false) { 
-			$('.addresses').get(0).mobile = true;
-			add_elements_addresses();
-			init_computer_slider('.addresses .list');
-		} else if($('.addresses').get(0).mobile == true) {
-			update_computer_slider('.addresses .list');
-		}
-	} else {
-		if($('.addresses').get(0).mobile == true) { 
-			$('.addresses').get(0).mobile = false;
-			remove_elements_addresses();
-		}
-	}
+	// if(window.matchMedia('(max-width: 480px)').matches) {
+	// 	if($('.addresses').get(0).mobile == false) {
+	// 		$('.addresses').get(0).mobile = true;
+	// 		add_elements_addresses();
+	// 		init_computer_slider('.addresses .list');
+	// 	} else if($('.addresses').get(0).mobile == true) {
+	// 		update_computer_slider('.addresses .list');
+	// 	}
+	// } else {
+	// 	if($('.addresses').get(0).mobile == true) {
+	// 		$('.addresses').get(0).mobile = false;
+	// 		remove_elements_addresses();
+	// 	}
+	// }
 }
